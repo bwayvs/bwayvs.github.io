039e6c2770de2092ec4d16587123e8ffffc696c8
bwayvs <veronica.broadway4@gmail.com>
2019-04-28 15:19:34 -0500

    Delete Welcome Message

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -15,17 +15,3 @@ document.querySelector('html').onclick = function() {
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1'); 
 
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'It is nice to meet you, ' + myName;
-  }
-  if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcome back, ' + storedName;
-  }
-  myButton.onclick = function() {
-    setUserName();
-  }
