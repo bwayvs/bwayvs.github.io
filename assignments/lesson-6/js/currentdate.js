19168f6b077a6eb0d84c8427ce73ae6a7fafbeab
bwayvs <veronica.broadway4@gmail.com>
2019-06-04 21:52:19 -0500

    Update Site

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,14 +1,15 @@
 function renderTime() {
 
-var today = new Date();
-var DD = today.getDay();
-var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-var dd = today.getDate();
-var mm = today.getMonth() +1;
-var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-var yyyy = today.getFullYear();
-var myClock = document.getElementById("clockDisplay");
-myClock.textContent = " " +dayarray[DD]+ " " +dd+ " " +montharray[mm]+ " " +yyyy+ " ";
-myClock.innerText = " " +dayarray[DD]+ " " +dd+ " " +montharray[mm]+ " " +yyyy+ " ";
-}
-renderTime();
+    var today = new Date();
+    var DD = today.getDay();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var dd = today.getDate();
+    var mm = today.getMonth() +1;
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var yyyy = today.getFullYear();
+    var myClock = document.getElementById("clockDisplay");
+    
+    myClock.innerText = " " +dayarray[DD]+ " " +dd+ " " +montharray[mm]+ " " +yyyy+ " ";
+    }
+    renderTime();
+    
