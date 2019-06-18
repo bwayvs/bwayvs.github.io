abf3e31e8d522ce205e59fce7b8ca52309d59be5
bwayvs <veronica.broadway4@gmail.com>
2019-06-17 22:28:02 -0500

    Update Site

--- assignments/lesson-9/js/towndata.js
+++ assignments/lesson-9/js/towndata.js
@@ -1,16 +1,17 @@
 
-var requestURL = 'https://bwayvs.github.io/assignments/lesson-9/js/towndata.json';
-var request = new XMLHttpRequest();
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const request = new XMLHttpRequest();
 request.open('GET', requestURL);request.responseType = 'json';
 
 request.send();
 
 request.onload = function() {
-    var townName = request.response;
-    populateH3(townName);
+    let townData = request.response;
+    let towns = townData['towns'];
     
-function populateH3(jsonObj) {
-    var myH3 = document.createElement('h3');
-    myH3.textContent = jsonObj['town':'name'[0]];
-    h3.appendChild(myH3);
+    towns.forEach(town => {
+        if (town.name == 'Preston') {
+            document.getElementById('prestonname').innerHTML = town.name;
+        }
+    });
 }
