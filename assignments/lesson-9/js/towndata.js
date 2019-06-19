8417d84d39c3d810b3199bdbffcfdca383dd041a
bwayvs <veronica.broadway4@gmail.com>
2019-06-18 21:08:24 -0500

    Update Site

--- assignments/lesson-9/js/towndata.js
+++ assignments/lesson-9/js/towndata.js
@@ -14,21 +14,21 @@ request.onload = function () {
         if (town.name == 'Preston') {
             document.getElementById('prestonname').innerHTML = town.name;
             document.getElementById('prestonmotto').innerHTML = town.motto;
-            document.getElementById('prestonyr').innerHTML = town.yearFounded;
+            document.getElementById('prestonyf').innerHTML = town.yearFounded;
             document.getElementById('prestonpop').innerHTML = town.currentPopulation;
             document.getElementById('prestonrain').innerHTML = town.averageRainfall;
         }
         if (town.name == 'Soda Springs') {
             document.getElementById('sodaspringsname').innerHTML = town.name;
             document.getElementById('sodaspringsmotto').innerHTML = town.motto;
-            document.getElementById('sodaspringsyr').innerHTML = town.yearFounded;
+            document.getElementById('sodaspringsyf').innerHTML = town.yearFounded;
             document.getElementById('sodaspringspop').innerHTML = town.currentPopulation;
             document.getElementById('sodaspringsrain').innerHTML = town.averageRainfall;
         }
         if (town.name == 'Fish Haven') {
             document.getElementById('fishhavenname').innerHTML = town.name;
             document.getElementById('fishhavenmotto').innerHTML = town.motto;
-            document.getElementById('fishhavenyr').innerHTML = town.yearFounded;
+            document.getElementById('fishhavenyf').innerHTML = town.yearFounded;
             document.getElementById('fishhavenpop').innerHTML = town.currentPopulation;
             document.getElementById('fishhavenrain').innerHTML = town.averageRainfall;
         }
