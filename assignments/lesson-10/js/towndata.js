fb6f2de7063765255c7526998f08e4a4be005d3b
bwayvs <veronica.broadway4@gmail.com>
2019-06-23 15:36:49 -0500

    Update Site

---
+++ assignments/lesson-10/js/towndata.js
@@ -0,0 +1,36 @@
+// ******************************
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+//****************************************/
+
+request.onload = function () {
+    let townData = request.response;
+    let towns = townData['towns'];
+
+    towns.forEach(town => {
+        if (town.name == 'Preston') {
+            document.getElementById('prestonname').innerHTML = town.name;
+            document.getElementById('prestonmotto').innerHTML = town.motto;
+            document.getElementById('prestonyf').innerHTML = town.yearFounded;
+            document.getElementById('prestonpop').innerHTML = town.currentPopulation;
+            document.getElementById('prestonrain').innerHTML = town.averageRainfall;
+        }
+        if (town.name == 'Soda Springs') {
+            document.getElementById('sodaspringsname').innerHTML = town.name;
+            document.getElementById('sodaspringsmotto').innerHTML = town.motto;
+            document.getElementById('sodaspringsyf').innerHTML = town.yearFounded;
+            document.getElementById('sodaspringspop').innerHTML = town.currentPopulation;
+            document.getElementById('sodaspringsrain').innerHTML = town.averageRainfall;
+        }
+        if (town.name == 'Fish Haven') {
+            document.getElementById('fishhavenname').innerHTML = town.name;
+            document.getElementById('fishhavenmotto').innerHTML = town.motto;
+            document.getElementById('fishhavenyf').innerHTML = town.yearFounded;
+            document.getElementById('fishhavenpop').innerHTML = town.currentPopulation;
+            document.getElementById('fishhavenrain').innerHTML = town.averageRainfall;
+        }
+    });
+}
---
+++ assignments/lesson-10/script/weatherapi.js
@@ -0,0 +1,16 @@
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=e49a6f6568d868f47ba5cc7a896e646f";
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.getElementById("cc-temp").innerHTML = weatherData.main.temp;
+
+    let desc = weatherData.weather[0].description;
+    document.getElementById("cc-img").setAttribute("alt", desc);
+
+    let icon = "http://openweathermap.org/img/w/" +weatherData.weather[0].icon+ ".png";
+    document.getElementById("cc-img").setAttribute("src", icon);
+}
