40195ffa54471ba8304044a173794ab7d815a502
bwayvs <veronica.broadway4@gmail.com>
2019-06-23 23:08:31 -0500

    Update Site

--- assignments/lesson-10/js/prestonweatherapi.js
+++ assignments/lesson-10/js/prestonweatherapi.js
@@ -5,7 +5,6 @@ weatherRequest.send();
 
 weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherData);
     document.getElementById("hitemp").innerHTML = weatherData.main.temp_max;
     document.getElementById("currently").innerHTML = weatherData.weather[0].main;
     document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
@@ -16,4 +15,8 @@ weatherRequest.onload = function() {
     let s = parseInt(document.getElementById('windspeed').innerHTML);
     let result = 35.74 + (0.6215 * t) - (35.75 * s ** 0.16) + (0.4275 * t * s ** 0.16);
     document.getElementById('windchill').innerHTML = result.toFixed(1);
+}
+
+function newFunction(weatherData) {
+    console.log(weatherData);
 }
