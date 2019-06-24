2797f388d2abb5b97dbceced5e75239089af10c9
bwayvs <veronica.broadway4@gmail.com>
2019-06-23 23:11:29 -0500

    Update Site

--- assignments/lesson-10/js/prestonforecastapi.js
+++ assignments/lesson-10/js/prestonforecastapi.js
@@ -5,7 +5,6 @@ forecastRequest.send();
 
 forecastRequest.onload = function () {
     let forecastData = JSON.parse(forecastRequest.responseText);
-    console.log(forecastData);
 
     /*Weekdays*/
     let today = new Date();
