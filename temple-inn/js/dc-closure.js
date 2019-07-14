045f99b4dcdb4c62a7fd9ba3cf6ebb04407fbb19
bwayvs <veronica.broadway4@gmail.com>
2019-07-14 11:06:23 -0500

    Create Temple Page

---
+++ temple-inn/js/dc-closure.js
@@ -0,0 +1,22 @@
+const requestDcClosureDataURL = 'https://bwayvs.github.io/temple-inn/js/temple-closure.html';
+const requestDcClosureData = new XMLHttpRequest();
+requestDcClosureData.open('GET', requestDcClosureDataURL);
+requestDcClosureData.responseType = 'json';
+requestDcClosureData.send();
+//****************************************//
+
+requestDcClosureData.onload = function () {
+    let myDcClosureList = document.getElementById('dcclosureeventsul');
+    let dcClosureData = requestDcClosureData.response;
+    let dcclosures = dcClosureData['temples'];
+    dcclosures.forEach(dctemple => {
+        if (dctemple.name == 'DC') {
+            let dcClosureEvents = dctemple.closures;
+            for (let f = 0; f < dcClosureEvents.length; f++) {
+                let listDcClosureItem = document.createElement('li');
+                listDcClosureItem.textContent = dcClosureEvents[f];
+                myDcClosureList.appendChild(listDcClosureItem);
+            }
+        }
+    });
+}
