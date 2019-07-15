2e0be28f845d4cee99af9283ce422dd700a352af
bwayvs <veronica.broadway4@gmail.com>
2019-07-14 20:44:11 -0500

    Update JSON File

--- temple-inn/js/temple-data.js
+++ temple-inn/js/temple-data.js
@@ -17,8 +17,13 @@ function showData(jsonObj){
 
     for(let i = 0; i < temples.length; i++) {
         let myH3 = document.createElement('h3');
-        let myH4 = document.createElement('h4');
-        let myList = document.createElement('ul');
+        let myList1 = document.createElement('ul');
+        let myList2 = document.createElement('ul');
+        let myList3 = document.createElement('ul');
+        let myList4 = document.createElement('ul');
+        let myList5 = document.createElement('ul');
+        let myList6 = document.createElement('ul');
+        let myList7 = document.createElement('ul');
         let myPara1 = document.createElement('p');
         let myPara2 = document.createElement('p');
         let myPara3 = document.createElement('p');
@@ -33,17 +38,45 @@ function showData(jsonObj){
         myH3.textContent = temples[i].name;
         myImg.setAttribute('src', temples[i].photo);
         myImg.setAttribute('alt', temples[i].desc);
-        myPara1.textContent = "Address: " + temples[i].address;
-        myPara2.textContent = "Phone Number : " + temples[i].phone;
-        myPara3.textContent = "Email: " + temples[i].email;
-        myPara4.textContent = "Schedule: " + temples[i].schedule;
-        myPara5.textContent = "History: ";
+
+        myPara1.textContent = "Address: " ;
+        let address = temples[i].address;
+        for(let m = 0; m < address.length; m++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = address[m];
+            myList1.appendChild(listItem);
+        }
+        myPara2.textContent = "Phone Number : ";
+       
+        let phone = temples[i].phone;
+        for(let n = 0; n < phone.length; n++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = phone[n];
+            myList2.appendChild(listItem);
+        }
+        myPara3.textContent = "Email: " ;
+       
+        let email = temples[i].email;
+        for(let p = 0; p < email.length; p++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = email[p];
+            myList3.appendChild(listItem);
+        }
+        myPara4.textContent = "Schedule: ";
+
+        let schedule = temples[i].schedule;
+        for(let q = 0; q < schedule.length; q++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = schedule[q];
+            myList4.appendChild(listItem);
+        }
+        myPara5.textContent = "Temple History: ";
 
         let history = temples[i].history;
         for(let j = 0; j < history.length; j++) {
             let listItem = document.createElement('li');
             listItem.textContent = history[j];
-            myList.appendChild(listItem);
+            myList5.appendChild(listItem);
         }
 
         myPara6.textContent = "Services: ";
@@ -52,7 +85,7 @@ function showData(jsonObj){
         for(let k = 0; k < services.length; k++) {
             let listItem = document.createElement('li');
             listItem.textContent = services[k];
-            myList.appendChild(listItem);
+            myList6.appendChild(listItem);
         }
 
         myPara7.textContent = "Temple Events: ";
@@ -61,17 +94,26 @@ function showData(jsonObj){
         for(let l = 0; l < closures.length; l++) {
             let listItem = document.createElement('li');
             listItem.textContent = closures[l];
-            myList.appendChild(listItem);
+            myList7.appendChild(listItem);
         }
         
         myDiv.appendChild(myImg);
         myDiv.appendChild(myH3);
         myDiv.appendChild(myPara1);
+        myDiv.appendChild(myList1);
         myDiv.appendChild(myPara2);
+        myDiv.appendChild(myList2);
         myDiv.appendChild(myPara3);
+        myDiv.appendChild(myList3);
         myDiv.appendChild(myPara4);
+        myDiv.appendChild(myList4);
         myDiv.appendChild(myPara5);
-        myDiv.appendChild(myList);
+        myDiv.appendChild(myList5);
+        myDiv.appendChild(myPara6);
+        myDiv.appendChild(myList6);
+        myDiv.appendChild(myPara7);
+        myDiv.appendChild(myList7);
+       
         
 
         section.appendChild(myDiv);
