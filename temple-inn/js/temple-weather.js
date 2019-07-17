aafc92db175ff06f60b77e5a662e3cff2219ae21
bwayvs <veronica.broadway4@gmail.com>
2019-07-16 19:59:11 -0500

    Update Reservations Page

--- temple-inn/js/temple-weather.js
+++ temple-inn/js/temple-weather.js
@@ -1,18 +1,19 @@
 let index = ["4366164", "4560349", "4575352", "4487042"];
 let requestnew = new XMLHttpRequest();
 
-//let myP = document.createElement('p')
-(function loop(t, length){
+
+(function loop(t, length, resultArr){
     if (t>=length) {
         return;
     }
     let url = "https://api.openweathermap.org/data/2.5/weather?id=" + index[t] + "&units=imperial&APPID=e49a6f6568d868f47ba5cc7a896e646f";
+    requestnew.open('GET',url);
     requestnew.onreadystatechange = function(){
         if(requestnew.readyState === XMLHttpRequest.DONE && requestnew.status ===200) {
             let data = JSON.parse(requestnew.responseText);
-            console.log('-->' + t + "Temperature: " + data.main.temp.toFixed());
+            document.createElement('p').textContent = "Temperature: " + data.main.temp.toFixed() + '/xB0';
             resultArr.push(data.main.temp.toFixed());
-            loop(t+1, length, resultARR);
+            loop(t+1, length, resultArr);
         }
     }
     requestnew.send();
